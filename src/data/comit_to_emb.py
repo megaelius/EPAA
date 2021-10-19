f0fdd3362b6b704d05f53b2104d591f70ec8bb10
andreagarzv <garciavaldesandrea@gmail.com>
2021-10-19 15:17:18 +0200

    comit_to_emb.py fino

--- src/data/comit_to_emb.py
+++ src/data/comit_to_emb.py
@@ -3,14 +3,25 @@ import numpy as np
 from tqdm import tqdm
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('all-mpnet-base-v2')
+
+@click.command()
+@click.argument('input_path', type=click.Path(exists=True))
+@click.argument('output_path', type=click.Path())
+
+def main(input_path, output_path):
+
+    final_db = pd.read_csv(input_path + '/predictionDB.csv',lineterminator='\n')
+    nrows = final_db.shape[0]
+
+    if not Path(output_path + '/embeddings').exists():
+        Path(output_path + '/embeddings').mkdir()
+
+    for index,row in tqdm(final_db.iterrows()):
+        frases = row['COMMIT_MESSAGE']
+        matrix = model.encode(frases)
+        np.save(output_path +'/embeddings/' + row['COMMIT_HASH'] + '.npy',matrix)
 
 
-final_db = pd.read_csv('data/processed/predictionDB.csv',lineterminator='\n')
-
-nrows = final_db.shape[0]
-
-for index,row in tqdm(final_db.iterrows()):
-    frases = row['COMMIT_MESSAGE']
-    matrix = model.encode(frases)
-    np.save(f'data/processed/embeddings/' + row['COMMIT_HASH'] + '.npy',matrix)
+if __name__ == '__main__':
+    main()
